4021812aebbcd8bd97aff9fc090f9e6455f63cb4
Bradley P. Allen <bradley.p.allen@gmail.com>
2012-09-20 13:37:54 -0700

    Fixed parse_svmlight_mapper.py to ignore comment lines (i.e. lines starting with '#')
    Now testing for line[0] != '#'.

--- parse_svmlight_mapper.py
+++ parse_svmlight_mapper.py
@@ -4,14 +4,15 @@ import sys, json, random, uuid
 
 def main():
     for line in sys.stdin:
-        example = line[0:line.find('#')].strip().split(' ')
-        id = str(uuid.uuid1())
-        klass = 0 if example[0] == '-1' else 1
-        random_key = random.random()
-        features = {}
-        for fv in [ pair.split(':') for pair in example[1:] ]:
-            features[fv[0]] = float(fv[1])
-        print "%17.16f\t%s\t%d\t%s" % (random_key, id, klass, json.dumps(features))
+        if line[0] != '#':
+            example = line[0:line.find('#')].strip().split(' ')
+            id = str(uuid.uuid1())
+            klass = 0 if example[0] == '-1' else 1
+            random_key = random.random()
+            features = {}
+            for fv in [ pair.split(':') for pair in example[1:] ]:
+                features[fv[0]] = float(fv[1])
+            print "%17.16f\t%s\t%d\t%s" % (random_key, id, klass, json.dumps(features))
     
 if __name__ == '__main__':
     main()
