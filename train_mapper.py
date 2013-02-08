9525587a343fc99e70eb298997d3e49dbde84c4b
Bradley P. Allen <bradley.p.allen@gmail.com>
2013-02-08 13:40:44 -0800

    Added ability to have more than one epoch in a training pass
    Changed train_mapper.py to explicitly use an environment variable, T,
    to set the number of iterations over the training set. This has the
    consequence of requiring the storage of all training instances in
    memory to support iteration; this will have to be taken into account in
    the provisioning of resources for execution. The main loop is closer in
    implementation to the pseudocode on page 6 in William Cohen's notes on
    stochastic gradient descent [1], and the learning rate is now decreased
    according to the formula in the second sentence of page 7 of that
    reference, as opposed to the previously utilized alternative of
    1/(number of examples).

--- train_mapper.py
+++ train_mapper.py
@@ -5,29 +5,31 @@ import collections, math, sys, json, os
 def main():
     mu = float(os.environ['MU']) if os.environ.has_key('MU') else 0.002
     eta = float(os.environ['ETA']) if os.environ.has_key('ETA') else 0.5
-    N = float(os.environ['N']) if os.environ.has_key('N') else 10000.
+    T = int(os.environ['T']) if os.environ.has_key('T') else 1
     split = float(os.environ['SPLIT']) if os.environ.has_key('SPLIT') else 0.3
     n_models_key = os.environ['N_MODELS_KEY'] if os.environ.has_key('N_MODELS_KEY') else 'MODEL'
-    t = 0
+    k = 0
     W = collections.defaultdict(float)
     A = collections.defaultdict(int)
-    for line in sys.stdin:
-        x = json.loads(line)
-        if x.has_key('class') and x["random_key"] > split:
-            sigma = sum([W[j] * x["features"][j] for j in x["features"].keys()])
-            p = 1. / (1. + math.exp(-sigma)) if -100. < sigma else sys.float_info.min
-            t += 1
-            lambd4 = eta / (1. + (float(t) / N))
-            penalty = 1. - (2. * lambd4 * mu)
-            for j in x["features"].keys():
-                W[j] *= math.pow(penalty, t - A[j])
-                W[j] += lambd4 * (float(x["class"]) - p) * x["features"][j]
-                A[j] = t
-    lambd4 = eta / (1. + (float(t) / N))
+    lines = [ line for line in sys.stdin ]
+    for t in range(1, T+1):
+        for line in lines:
+            x = json.loads(line)
+            if x.has_key('class') and x["random_key"] > split:
+                sigma = sum([W[j] * x["features"][j] for j in x["features"].keys()])
+                p = 1. / (1. + math.exp(-sigma)) if -100. < sigma else sys.float_info.min
+                k += 1
+                lambd4 = eta / float(pow(t, 2))
+                penalty = 1. - (2. * lambd4 * mu)
+                for j in x["features"].keys():
+                    W[j] *= math.pow(penalty, k - A[j])
+                    W[j] += lambd4 * (float(x["class"]) - p) * x["features"][j]
+                    A[j] = k
+    lambd4 = eta / float(pow(T, 2))
     penalty = 1. - (2. * lambd4 * mu)
     print "%s\t%17.16f" % (n_models_key, 1.)
     for j in W.keys():
-        W[j] *= math.pow(penalty, t - A[j])
+        W[j] *= math.pow(penalty, k - A[j])
         print "%s\t%17.16f" % (j, W[j])
     
 if __name__ == '__main__':
